67a51e201288c421ed2f6f1a2a16f320067fb3c1
dell <960647378@qq.com>
2020-09-04 23:39:11 +0800

    feet(客户管理):新建客户管理基础

--- laravel_pro/app/Admin/Controllers/CustomerController.php
+++ laravel_pro/app/Admin/Controllers/CustomerController.php
@@ -15,7 +15,7 @@ class CustomerController extends AdminController
      *
      * @var string
      */
-    protected $title = 'Customer';
+    protected $title = '客户管理-基本信息';
 
     /**
      * Make a grid builder.
@@ -26,8 +26,13 @@ class CustomerController extends AdminController
     {
         $grid = new Grid(new Customer());
 
-
-
+        $grid->column('id', 'ID')->sortable();
+        $grid->column('nickname', '用户名');
+        $grid->column('mobile', '手机号');
+        $grid->column('status', '状态')->display(function ($status) {
+            return $status ? '启用' : '禁用';
+        });
+        $grid->column('created_at', '创建时间');
         return $grid;
     }
 
@@ -40,8 +45,15 @@ class CustomerController extends AdminController
     protected function detail($id)
     {
         $show = new Show(Customer::findOrFail($id));
-
-
+        $show->field('id', 'ID');
+        $show->field('nickname', '用户名');
+        $show->field('mobile', '手机号');
+        $show->field('status', '状态')->as(function ($status) {
+            return $status ? '启用' : '禁用';
+        });
+        $show->field('mobile', '手机号');
+        $show->head_img_url('头像')->image();
+        $show->field('created_at', '创建时间');
 
         return $show;
     }
@@ -55,7 +67,16 @@ class CustomerController extends AdminController
     {
         $form = new Form(new Customer());
 
+        $form->display('id', 'ID');
+        $form->text('nickname', '用户名');
+        $form->mobile('mobile', '手机号');
 
+        $status = [
+            'on'  => ['value' => 1, 'text' => '启用', 'color' => 'success'],
+            'off' => ['value' => 0, 'text' => '禁用', 'color' => 'danger'],
+        ];
+        $form->image('head_img_url','头像')->uniqueName();
+        $form->switch('status', '状态')->states($status);
 
         return $form;
     }
